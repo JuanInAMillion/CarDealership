a4885489ee3fb11d23f0df5f1a6384fb4ac833ea
Juan <juan231f@yahoo.com>
2021-02-01 18:41:59 -0500

    added log4j dependencies

--- car_dealership/src/main/java/com/app/main/Driver.java
+++ car_dealership/src/main/java/com/app/main/Driver.java
@@ -2,46 +2,48 @@ package com.app.main;
 
 import java.util.Scanner;
 
+import org.apache.log4j.Logger;
+
 import com.app.model.Customer;
 import com.app.model.Employee;
 
-public class CarDealership {
+public class Driver {
+	private static Logger log=Logger.getLogger("consoleLog.Driver");
 	public static void main(String[] args) {
 		
 		Customer customer = null;
 		Employee employee = null;
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Welcome to Chase Bank");
+		System.out.println("Welcome to Larusso's Car Dealership");
 		System.out.println("-----------------------");
 		int ch = 0;
 		do {
-			System.out.println("\nChoose an Options");
+			System.out.println("Choose an Options");
 			System.out.println("=======================");
 			System.out.println("1)Login");
 			System.out.println("2)Register");
 			System.out.println("3)Employee Login");
-			System.out.println("4)EXIT");
-			System.out.println("");
+			System.out.println("4)EXIT\n");
 			try {
 				ch=Integer.parseInt(sc.nextLine());
 			} catch(NumberFormatException e) {}
 			
 			switch(ch) {
 			case 1:
-				System.out.println("Login");
+				System.out.println("Login\n");
 				break;
 			case 2:
-				System.out.println("Register");
+				System.out.println("Register\n");
 				break;
 			case 3:
-				System.out.println("Employee Login");
+				System.out.println("Employee Login\n");
 				break;
 			case 4:
-				System.out.println("\nThank You For Visiting Larusso's Car Dealership, have a nice day!\n");
+				System.out.println("Thank You For Visiting Larusso's Car Dealership, have a nice day!\n");
 				break;
 			default:
-				System.out.println("\nInvalid Menu Option. Choose from the given Options.\n");
+				System.out.println("Invalid Menu Option. Choose from the given Options.\n");
 				break;
 			}
 		} while(ch != 4);
